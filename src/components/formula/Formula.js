10ad2f55a9a3efa8779890b8318b24d914a78ba9
vitekrom <vitekrom@outlook.com>
2020-09-07 13:48:43 +0700

    finish base framework functional

---
+++ src/components/formula/Formula.js
@@ -0,0 +1,32 @@
+import {ExcelComponent} from '@core/ExcelComponent';
+
+export class Formula extends ExcelComponent {
+    static className = 'exsel__formula'
+
+    constructor($root) {
+      super($root, {
+        name: 'Formula',
+        listeners: ['input', 'remove', 'click']
+      })
+    }
+
+    toHTML() {
+      return `
+        <div class="info">fx</div>
+        <div class="input" contenteditable="" spellcheck="false" ></div>
+        `
+    }
+
+    onInput(event) {
+      console.log(this.$root);
+      console.log('Formula: onInput', event);
+    }
+    onClick() {
+      console.log('cclic')
+    }
+
+    onRemove(event) {
+      console.log('aasic')
+      console.log('Formula: onRemove', event);
+    }
+}
